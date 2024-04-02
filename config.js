93b7248013c9b94eb4f17602f42af2d0aade43c1
Marc Vanbrabant <slyoldfox@gmail.com>
2024-04-02 17:44:15 +0200

    Add some basic support to publish events to an external MQTT server

---
+++ config.js
@@ -0,0 +1,32 @@
+const doorUnlock = {
+    // Default behaviour is device ID 20, if you need more, add them to additionalLocks below
+    openSequence: '*8*19*20##' ,
+    closeSequence: '*8*20*20##',
+};
+
+const additionalLocks = {
+    // Uncomment this if you have extra locks, you can call them with /unlock?id=side-door
+    //"back-door": { openSequence: '*8*19*21##', closeSequence: '*8*19*21##' },
+    //"side-door": { openSequence: '*8*19*22##', closeSequence: '*8*19*22##' }
+};
+
+const mqtt_config = {
+    // Set to enable to publish events to an external MQTT server
+    'enabled': false,
+    // Publish all openwebnet events (can be noisy and overload your system?)
+    'all_events_enabled': false,
+    // Hostname or IP of the external MQTT server
+    'host': '',
+    'port': 1883,
+    // If anonymous MQTT leave blank
+    'username': '',
+    'password': '',
+    // MQTT Topic, will resolve to 'topic/eventname'
+    'topic': 'bticino',
+    // Path of mosquitto_pub on the intercom
+    'exec_path': '/usr/bin/mosquitto_pub'
+}
+
+module.exports = {
+    doorUnlock , additionalLocks, mqtt_config
+}
--- controller.js
+++ controller.js
@@ -5,8 +5,9 @@ const Api = require('./lib/api')
 const MulticastListener = require("./lib/multicast-listener");
 const udpProxy = require('./lib/udp-proxy')
 const EndpointRegistry = require('./lib/endpoint-registry')
+const mqtt = require('./lib/mqtt')
 
 const registry = EndpointRegistry.create()
 const api = Api.create(registry)
 udpProxy.create()
-MulticastListener.create(registry, api)
+MulticastListener.create(registry, api, mqtt.create())
