1b920c7014271419bd4d862180ed2997d908af81
Marc Vanbrabant <slyoldfox@gmail.com>
2024-05-22 08:08:46 +0200

    Added Webrtc support Added Homekit support for locks, (un)mute, voicemail (c300x) Validate setup includes model

--- controller.js
+++ controller.js
@@ -1,16 +1,3 @@
 #!/usr/bin/env node
 
-"use strict";
-var package_json = require('./package.json');
-
-console.log(`======= c300x-controller ${package_json.version} for use with BTicino plugin 0.0.15 =======`)
-const Api = require('./lib/api')
-const MulticastListener = require("./lib/multicast-listener");
-const udpProxy = require('./lib/udp-proxy')
-const EndpointRegistry = require('./lib/endpoint-registry')
-const mqtt = require('./lib/mqtt')
-
-const registry = EndpointRegistry.create()
-const api = Api.create(registry)
-udpProxy.create( 40004, '0.0.0.0', 4000, '127.0.0.1' )
-MulticastListener.create(registry, api, mqtt.create(api))
+require('./base')
